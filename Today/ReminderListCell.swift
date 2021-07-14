7a695e08cd7ff79cc64fef2405a052da49f09623
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-07-14 17:55:41 +0300

    Connect object storyboard to class 'ReminderListCell'

---
+++ Today/ReminderListCell.swift
@@ -0,0 +1,19 @@
+//
+//  ReminderListCell.swift
+//  Today
+//
+//  Created by Sergey Lukaschuk on 14.07.2021.
+//
+
+
+import UIKit
+
+class ReminderListCell: UITableViewCell {
+    
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
+    
+    
+}
