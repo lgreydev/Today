bc7b189950264517c6e321d2963c11e702109ab8
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-07-14 11:23:07 +0300

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Sergey Lukaschuk on 14.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
