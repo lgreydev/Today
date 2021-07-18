537a48f03ebbbd5ac5968fa449aaf54e9ed22242
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-07-18 23:16:56 +0300

    Add configure 'ReminderListCell'

--- Today/ReminderListCell.swift
+++ Today/ReminderListCell.swift
@@ -15,10 +15,19 @@ class ReminderListCell: UITableViewCell {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var doneButton: UIButton!
-
-    var doneButtonAction: DoneButtonAction?
+    
+    private var doneButtonAction: DoneButtonAction?
     
     @IBAction func doneButtonTriggered(_ sender: UIButton) {
-            doneButtonAction?()
-        }
+        doneButtonAction?()
+    }
+    
+    func configure(title: String, dateText: String, isDone: Bool, doneButtonAction: @escaping DoneButtonAction) {
+        titleLabel.text = title
+        dateLabel.text = dateText
+        let image = isDone ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
+        doneButton.setBackgroundImage(image, for: .normal)
+        self.doneButtonAction = doneButtonAction
+        
+    }
 }
