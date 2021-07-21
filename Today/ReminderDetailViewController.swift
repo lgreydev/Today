838e9da059132fd0d4d278e67d32b48932c9000b
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-07-21 23:00:26 +0300

    Configure the cancel button

--- Today/ReminderDetailViewController.swift
+++ Today/ReminderDetailViewController.swift
@@ -10,18 +10,18 @@ import UIKit
 class ReminderDetailViewController: UITableViewController {
     private var reminder: Reminder?
     private var dataSource: UITableViewDataSource?
-
+    
     func configure(with reminder: Reminder) {
         self.reminder = reminder
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setEditing(false, animated: false)
         navigationItem.setRightBarButton(editButtonItem, animated: false)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: ReminderDetailEditDataSource.dateLabelCellIdentifier)
     }
-
+    
     override func setEditing(_ editing: Bool, animated: Bool) {
         super.setEditing(editing, animated: animated)
         guard let reminder = reminder else {
@@ -29,11 +29,20 @@ class ReminderDetailViewController: UITableViewController {
         }
         if editing {
             dataSource = ReminderDetailEditDataSource(reminder: reminder)
+            navigationItem.title = NSLocalizedString("Edit Reminder", comment: "edit reminder nav title")
+            navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonTrigger))
         } else {
             dataSource = ReminderDetailViewDataSource(reminder: reminder)
+            navigationItem.title = NSLocalizedString("View Reminder", comment: "view reminder nav title")
+            navigationItem.leftBarButtonItem = nil
+            editButtonItem.isEnabled = true
         }
         tableView.dataSource = dataSource
         tableView.reloadData()
     }
+    
+    @objc
+    func cancelButtonTrigger() {
+        setEditing(false, animated: true)
+    }
 }
-//dateLabelCellIdentifier
